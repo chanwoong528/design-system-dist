53aaea6dfbfff9b7d7334084b40d21388ddfe2f3
mooncw@hankookilbo.com <mooncw@hankookilbo.com>
2024-12-09 13:52:16 +0900

    Add generated CSS file 2024. 12. 9. 오후 1:52:16

---
+++ jsx/react/typo.jsx
@@ -0,0 +1,16 @@
+
+        import React from "react";
+
+        const typo = ({children, tag, ...restProps}) => {
+
+        const cssList = ["b2","b1","d1","d3","d2"];
+        const css = restProps.className ? restProps.className : cssList[0];
+     
+        if (!!tag) {
+          return React.createElement(tag, {...restProps,className: css }, children);
+        }
+
+        return <p {...restProps}>{children}</p>;
+        };
+        export default typo;
+      
